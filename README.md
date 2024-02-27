bc935272b1d3308f832d4c654b51681a345e20c4
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2024-02-27 18:30:58 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Web3Auth Wallet Ecosystems Examples
+
+[Web3Auth](https://web3auth.io) is a plugable auth infrastructure for Web3
+wallets and applications. It streamlines the onboarding of both mainstream and
+crypto native users under a minute by providing experiences that they're most
+comfortable with.
+
+With support for all social logins, web & mobile native platforms, wallets and
+other key management methods, Web3Auth results in a standard cryptographic key
+provider specific to the user and application.
+
+## Wallet Ecosystems
+
+Wallet Ecosystems are flagship integrations of Web3Auth from established Web3 communities, which are available for seamless integrations within your own application. This innovative platform leverages the Web3Auth infrastructure for efficient key management, while offering developers a straightforward and user-friendly interface, to quickly integrate complex features of Web3Auth within seconds, while getting access to the userbase of existing communities. This integration facilitates essential features such as MPC with Account Abstraction, ensuring a smooth and unified user experience across different platforms.
+
+This repository contains integration examples for the following Web3Auth Wallet Ecosystems:
+- [SafeAuth](https://docs.safe.global/sdk-auth-kit/guides/safe-auth)
+- [Mocaverse ID](https://account.mocaverse.xyz/)
+- [Torus EVM Wallet](https://app.tor.us/)
+- [Torus Solana Wallet](https://solana.tor.us/login)
+
+## Important Links
+
+- [Website](https://web3auth.io)
+- [Docs](https://web3auth.io/docs)
+- [SDK / API References](https://web3auth.io/docs/sdk)
+- [Web3Auth Community](https://web3auth.io/community/)
+- [Guides](https://web3auth.io/docs/content-hub?type=guides)
---
+++ mocaverse-embed/moca-react-example/src/App.tsx
@@ -0,0 +1,200 @@
+import { useEffect, useState } from "react";
+import MocaEmbed from "@web3auth/mocaverse-embed";
+import { WEB3AUTH_NETWORK, CHAIN_NAMESPACES } from "@web3auth/base";
+import Web3 from "web3";
+
+import "./App.css";
+
+const web3AuthClientId =
+  "BANbxuTYFGeYi8HxUzaPQkvQlSAXiKRtUqb1vqsXbsZsZKrNr05PEPCM2J2PhUJZpIYl0XzQa6jxUjnYzSU9LXY"; // get from https://dashboard.web3auth.io
+
+const mocaEmbed = new MocaEmbed({
+  web3AuthClientId,
+  web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_MAINNET,
+});
+
+const chainConfig = {
+  chainNamespace: CHAIN_NAMESPACES.EIP155,
+  chainId: "0x89", // hex of 137, polygon mainnet
+  rpcTarget: "https://rpc.ankr.com/polygon",
+  // Avoid using public rpcTarget in production.
+  // Use services like Infura, Quicknode etc
+  displayName: "Polygon Mainnet",
+  blockExplorerUrl: "https://polygonscan.com",
+  ticker: "MATIC",
+  tickerName: "MATIC",
+  logo: "https://cryptologos.cc/logos/polygon-matic-logo.png",
+};
+
+function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        await mocaEmbed.init({
+          buildEnv: "testing",
+          chainConfig,
+          enableLogging: true,
+          whiteLabel: {
+            showWidgetButton: true,
+            buttonPosition: "bottom-left",
+          }
+        });
+
+        if (mocaEmbed.provider.isConnected()) {
+          setLoggedIn(true);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    init();
+  }, []);
+
+  const login = async () => {
+    const accounts = await mocaEmbed.login();
+    uiConsole(accounts);
+  };
+
+  const getUserInfo = async () => {
+    const user = mocaEmbed.getUserInfo();
+    uiConsole(user);
+  };
+
+  const logout = async () => {
+    // IMP START - Logout
+    await mocaEmbed.logout();
+    // IMP END - Logout
+    setLoggedIn(false);
+    uiConsole("logged out");
+  };
+
+  // IMP START - Blockchain Calls
+  const getAccounts = async () => {
+    if (!loggedIn) {
+      uiConsole("Not logged in");
+      return;
+    }
+    const web3 = new Web3(mocaEmbed.provider as any);
+
+    // Get user's Ethereum public address
+    const address = await web3.eth.getAccounts();
+    uiConsole(address);
+  };
+
+  const getBalance = async () => {
+    if (!loggedIn) {
+      uiConsole("Not logged in");
+      return;
+    }
+    const web3 = new Web3(mocaEmbed.provider as any);
+
+    // Get user's Ethereum public address
+    const address = (await web3.eth.getAccounts())[0];
+
+    // Get user's balance in ether
+    const balance = web3.utils.fromWei(
+      await web3.eth.getBalance(address), // Balance is in wei
+      "ether"
+    );
+    uiConsole(balance);
+  };
+
+  const signMessage = async () => {
+    if (!loggedIn) {
+      uiConsole("Not logged in");
+      return;
+    }
+    const web3 = new Web3(mocaEmbed.provider as any);
+
+    // Get user's Ethereum public address
+    const fromAddress = (await web3.eth.getAccounts())[0];
+
+    const originalMessage = "YOUR_MESSAGE";
+
+    // Sign the message
+    const signedMessage = await web3.eth.personal.sign(
+      originalMessage,
+      fromAddress,
+      "test password!" // configure your own password here.
+    );
+    uiConsole(signedMessage);
+  };
+  // IMP END - Blockchain Calls
+
+  function uiConsole(...args: any[]): void {
+    const el = document.querySelector("#console>p");
+    if (el) {
+      el.innerHTML = JSON.stringify(args || {}, null, 2);
+    }
+    console.log(...args);
+  }
+
+  const loggedInView = (
+    <>
+      <div className="flex-container">
+        <div>
+          <button onClick={getUserInfo} className="card">
+            Get User Info
+          </button>
+        </div>
+        <div>
+          <button onClick={getAccounts} className="card">
+            Get Accounts
+          </button>
+        </div>
+        <div>
+          <button onClick={getBalance} className="card">
+            Get Balance
+          </button>
+        </div>
+        <div>
+          <button onClick={signMessage} className="card">
+            Sign Message
+          </button>
+        </div>
+        <div>
+          <button onClick={logout} className="card">
+            Log Out
+          </button>
+        </div>
+      </div>
+    </>
+  );
+
+  const unloggedInView = (
+    <button onClick={login} className="card">
+      Login
+    </button>
+  );
+
+  return (
+    <div className="container">
+      <h1 className="title">
+        <a target="_blank" href="https://web3auth.io/docs/sdk/pnp/web/modal" rel="noreferrer">
+          Mocaverse Embed{" "}
+        </a>
+        & ReactJS (Webpack) Quick Start
+      </h1>
+
+      <div className="grid">{loggedIn ? loggedInView : unloggedInView}</div>
+      <div id="console" style={{ whiteSpace: "pre-line" }}>
+        <p style={{ whiteSpace: "pre-line" }}></p>
+      </div>
+
+      <footer className="footer">
+        <a
+          href="https://github.com/Web3Auth/wallet-ecosystems-examples/tree/main/mocaverse-embed/moca-react-example/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Source code
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
